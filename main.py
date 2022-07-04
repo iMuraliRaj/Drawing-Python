5e9ddebc03bbc7baaff1ddb181c37152379b4f4d
ATE186 <87387589+ATE186@users.noreply.github.com>
2022-07-04 14:31:25 +0530

    a

---
+++ main.py
@@ -0,0 +1,13 @@
+# Python program to draw square
+# using Turtle Programming
+import turtle
+
+a = turtle.Turtle()
+
+a.right(120)
+a.forward(120)
+a.left(110)
+a.forward(20)
+a.left(30)
+a.forward(50)
+turtle.done()
